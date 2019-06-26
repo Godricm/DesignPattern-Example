73555d03ff77e5e3274b5c67e7a61879a4f677c7
godric <miaochengod@outlook.com>
2019-06-26 20:58:07 +0800

    [add] 组合模式

---
+++ CompositePattern/src/org/buding/Employee.java
@@ -0,0 +1,42 @@
+package org.buding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: DesignPattern-Example
+ * @author: miaochen
+ * @create: 2019-06-26 14:23
+ * @description:
+ **/
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates=new ArrayList<>();
+    }
+
+    public void add(Employee employee){
+        subordinates.add(employee);
+    }
+
+    public void remove(Employee e){
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
